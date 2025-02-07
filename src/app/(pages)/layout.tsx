a22deab14b23449532e151df3a3b13716ba5472c
Muzaffar Ali <muzafark834@gmail.com>
2025-02-08 01:48:36 +0500

    solved

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import DashboardNav from "../navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+  
     <html lang="en">
       <body className={inter.className}>
        <DashboardNav/>
         {children}</body>
     </html>
-    </ClerkProvider>
+   
   );
 }
